c4b8b6b889e11c3f6a0b522bb9b9a8d1d95c76bf
yuk7 <yukx00@gmail.com>
2021-12-11 00:30:51 +0900

    add xposed scope to manifest

--- app/src/main/java/com/github/yuk7/xposed/pixelizerx/hooks/BuildPropHook.kt
+++ app/src/main/java/com/github/yuk7/xposed/pixelizerx/hooks/BuildPropHook.kt
@@ -1,14 +1,8 @@
 package com.github.yuk7.xposed.pixelizerx.hooks
 
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook.MethodHookParam
-
-import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
-
 import de.robv.android.xposed.XposedHelpers
-
-import de.robv.android.xposed.callbacks.XC_LoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
 
@@ -18,7 +12,7 @@ class BuildPropHook: IXposedHookLoadPackage {
             return
         }
         XposedBridge.log("loaded pkg=" + lpparam.packageName)
-        var hookClass = XposedHelpers.findClass("android.os.Build", lpparam?.classLoader)
+        val hookClass = XposedHelpers.findClass("android.os.Build", lpparam.classLoader)
         XposedHelpers.setStaticObjectField(hookClass, "BOARD" , "HOOKED_BOARD")
         XposedHelpers.setStaticObjectField(hookClass, "BOOTLOADER", "HOOKED_BL")
         XposedHelpers.setStaticObjectField(hookClass, "BRAND", "HOOKED_BRAND")
@@ -27,8 +21,6 @@ class BuildPropHook: IXposedHookLoadPackage {
         XposedHelpers.setStaticObjectField(hookClass, "HARDWARE", "HOOKED_HARDWARE")
         XposedHelpers.setStaticObjectField(hookClass, "MANUFACTURER", "HOOKED_MANUFACTURER")
         XposedHelpers.setStaticObjectField(hookClass, "MODEL", "HOOKED_MODEL")
-
-
     }
 
 }
